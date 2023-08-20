b95fc6e4b308f7a5d8ad5665e7b8fce481f1d6fd
Md Asifur Rahman <asifur94@gmail.com>
2023-08-20 13:12:16 +0600

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+(function($) {
+    "use strict";
+
+    const showData = document.getElementById('checked');
+
+    showData.addEventListener('click', function(){
+        $("#show_file").css('display', 'block');
+    })
+
+
+})($);
